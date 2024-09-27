78f69b281f410d55363f5f26a9c927f38913588e
Bryanz28 <bryanz28@nycstudents.net>
2024-09-27 15:49:14 +0000

    secondf commit

--- src/main/java/com/example/project/ExtraCredit.java
+++ src/main/java/com/example/project/ExtraCredit.java
@@ -1,31 +1,123 @@
+/*
+my source:
+https://www.w3schools.com/java/ref_math_round.asp
+*/
+
+
+
+
 package com.example.project;
-import java.util.Scanner;
 
 
-public class ExtraCredit {
-
-
-    public static String calculateTip(int people, int percent, double cost, String items) {
+public class TipCalculator {
+    //WRITE YOUR PROGRAM IN calculateTip
+    public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
+        //DO NOT DELETE ANY OF THE CODE BELOW      
         StringBuilder result = new StringBuilder();
+        //your code here
+       
+        double tipAmt;  
+        tipAmt = (cost*(percent/100.0));
+        double totalWtip = tipAmt+ cost;
+        double perCostBeforeTip = cost/people;
+        double TipPerPerson = tipAmt/people;
+        double CostPerPerson = totalWtip/people;
 
-                //COPY AND PASTE YOUR CODE HERE 
 
-        //the two lines  should go below result.append("-------------------------------\n"); 
-        result.append("Items ordered:\n");
-        result.append(items);
+        result.append("-------------------------------\n");
+        result.append("Total bill before tip: $"+cost +"\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: "+percent+"%"+"\n");
+        result.append("Total tip: $"+ Math.round(tipAmt*100)/100.0+"\n");
+        result.append("Total Bill with tip: $"+Math.round(totalWtip*100)/100.0+"\n");
+        result.append("Per person cost before tip: $"+Math.round(perCostBeforeTip*100)/100.0+"\n");
+        result.append("Tip per person: $"+Math.round(TipPerPerson*100)/100.0+"\n");
+        result.append("Total cost per person: $"+Math.round(CostPerPerson*100)/100.0+"\n");
+        result.append("-------------------------------\n");
+
+
 
 
         return result.toString();
     }
-                                   
-    public static void main(String[] args) {
-        int people;
-        int percent;
-        double cost;
-        String items; 
-
-        //Your scanner object and while loop should go here
-                             
-        System.out.println(calculateTip(people,percent,cost,items));
+     //TEST YOUR PROGRAM IN main
+     public static void main(String[] args) {
+       
+       
+        int people = 6;
+        int percent = 25;
+        double cost = 52.27;              
+        System.out.println(calculateTip(people,percent,cost));
     }
 }
+       
+
+
+
+
+
+
+
+
+
+
+package com.example.project;
+import java.util.Scanner;
+
+
+public class extraCredit {
+ 
+    public static String calculateTip(int people, int percent, double cost, String var2) { //You must use these  variable in your calculations
+        //DO NOT DELETE ANY OF THE CODE BELOW      
+        StringBuilder result = new StringBuilder();
+        //your code here
+       
+        double tipAmt;  
+        tipAmt = (cost*(percent/100.0));
+        double totalWtip = tipAmt+ cost;
+        double perCostBeforeTip = cost/people;
+        double TipPerPerson = tipAmt/people;
+        double CostPerPerson = totalWtip/people;
+
+
+        result.append("-------------------------------\n");
+        result.append("Total bill before tip: $"+cost +"\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total percentage: "+percent+"%"+"\n");
+        result.append("Total tip: $"+ Math.round(tipAmt*100)/100.0+"\n");
+        result.append("Total Bill with tip: $"+Math.round(totalWtip*100)/100.0+"\n");
+        result.append("Per person cost before tip: $"+Math.round(perCostBeforeTip*100)/100.0+"\n");
+        result.append("Tip per person: $"+Math.round(TipPerPerson*100)/100.0+"\n");
+        result.append("Total cost per person: $"+Math.round(CostPerPerson*100)/100.0+"\n");
+        result.append("-------------------------------\n");
+        result.append("Items ordered:\n" + var2);
+
+
+        return result.toString();
+    }
+      //TEST YOUR PROGRAM IN main
+      public static void main(String[] args) {
+       
+       
+        int people = 6;
+        int percent = 25;
+        double cost = 52.27;
+        Scanner scan = new Scanner(System.in);
+        String var;
+        String var2 = "";
+        while (true) {
+            System.out.println("Enter an item name or type '-1' to finish:");
+            var = scan.nextLine();
+            if (!var.equals("-1")) {
+                var2 += var + "\n";
+            }else{
+                break;
+            }
+        }
+        scan.close();
+        System.out.println(calculateTip(people,percent,cost,var2));
+       
+       
+    }
+}
+       
+
+
